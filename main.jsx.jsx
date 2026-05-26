78f8419ffedd50f1a872ee4dd691d416dc545066
Coolman0420 <132051670+Coolman0420@users.noreply.github.com>
2026-05-26 19:51:04 +0530

    Delete main.jsx

--- main.jsx.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BeyondSIEM from '../NX.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BeyondSIEM />
-  </React.StrictMode>,
-)
